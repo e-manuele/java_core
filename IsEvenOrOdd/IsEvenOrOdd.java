47031ab080989fe73a913c7087db134c63661275
Emanuele Altamura <altamuraemanuele@yahoo.it>
2023-06-22 17:00:07 +0200

    Aggiunta cartella e file java IsEvenOrOdd

---
+++ IsEvenOrOdd/IsEvenOrOdd.java
@@ -0,0 +1,13 @@
+public class IsEvenOrOdd{
+	public static void main(String[] args){
+	
+	int num = Integer.parseInt(args[0]);
+	if (num%2==0){
+		System.out.println("The number " +num+ " is even... You should know it.");
+	}
+	else {
+		System.out.println("The number " +num+ " is odd... You should know it.");
+	}
+    
+
+}}
